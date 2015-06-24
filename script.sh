27d991216618a3b26a81fb5a299ee2c37d4aa24e
Parc des Ecrins <pne@ecrins-parcnational.fr>
2015-06-24 15:14:38 +0200

    Ajout d'un fichier de script pour le renommage des fichiers

---
+++ script.sh
@@ -0,0 +1,13 @@
+chmod 775 -R cache
+chmod 775 -R templates_c
+mv config/config.php.sample config/config.php
+mv config/config_pages.php.sample config/config_pages.php
+mv templates/pages/fr/page_aide.tpl.sample templates/pages/fr/page_aide.tpl
+mv templates/pages/fr/page_contact.tpl.sample templates/pages/fr/page_contact.tpl
+mv templates/pages/fr/page_item_2.tpl.sample templates/pages/fr/page_item_2.tpl
+mv templates/pages/fr/page_legende.tpl.sample templates/pages/fr/page_legende.tpl
+mv templates/pages/fr/page_outils_informations.tpl.sample templates/pages/fr/page_outils_informations.tpl
+mv templates/pages/fr/page_sous_item_1.tpl.sample templates/pages/fr/page_sous_item_1.tpl
+mv templates/pages/fr/page_sous_item_2.tpl.sample templates/pages/fr/page_sous_item_2.tpl
+mv templates/pages/fr/page_sous_item_3.tpl.sample templates/pages/fr/page_sous_item_3.tpl
+mv templates/pages/fr/page_sous_item_4.tpl.sample templates/pages/fr/page_sous_item_4.tpl
