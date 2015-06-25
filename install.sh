1618de03f3ae75d551d4d35d7b59ccb40511075b
Parc des Ecrins <pne@ecrins-parcnational.fr>
2015-06-25 12:51:06 +0200

    correction de l'identification

--- install.sh
+++ install.sh
@@ -12,3 +12,10 @@ cp templates/pages/fr/page_sous_item_1.tpl.html.sample templates/pages/fr/page_s
 cp templates/pages/fr/page_sous_item_2.tpl.html.sample templates/pages/fr/page_sous_item_2.tpl.html
 cp templates/pages/fr/page_sous_item_3.tpl.html.sample templates/pages/fr/page_sous_item_3.tpl.html
 cp templates/pages/fr/page_sous_item_4.tpl.html.sample templates/pages/fr/page_sous_item_4.tpl.html
+cp images/034143.jpg.sample images/034143.jpg
+cp images/img1sousItem1.jpg.sample images/img1sousItem1.jpg
+cp images/img2sousItem1.jpg.sample images/img2sousItem1.jpg
+cp images/img3sousItem2.jpg.sample images/img3sousItem2.jpg
+cp images/img4item2.jpg.sample images/img4item2.jpg
+cp images/logo.jpg.sample images/logo.jpg
+cp images/logo.png.sample images/logo.png
