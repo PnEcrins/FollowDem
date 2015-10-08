70db3c1f90398a0c4903b4b1921af77741621a5d
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2015-10-08 16:42:37 +0200

    US language addition

---
+++ locale/us_US.php
@@ -0,0 +1,54 @@
+<?php
+/**
+*	Language file - US
+*	@author Camille Monchicourt
+*	@copyright Parc national des Écrins
+*	
+*/
+
+
+
+$lang['titre'] = 'FollowDem';
+
+
+
+
+/* Leaflet layers texts */
+$lang['Google Satellite'] = 'Google Satellite';
+$lang['Points'] = 'Points';
+$lang['Traces'] = 'Tracks';
+$lang['Derniers points'] = 'Last point';
+
+/*Pages menu*/
+$lang['Informations'] = 'Item 1';
+$lang['Bouquetin des Alpes'] = 'Subitem 1';
+$lang['Capture Bouquetins'] = 'Subitem 2';
+$lang['Faune sauvage'] = 'Subitem 3';
+$lang['Diego'] = 'Subitem 4';
+$lang['Actualites'] = 'All news'; // not used
+$lang['Parc national des Ecrins'] = 'Item 2';
+$lang['Outils'] = 'Tools';
+$lang['Réinitialiser la vue'] = 'Reset view';
+$lang['Légendes de la carte'] = 'Map legends';
+$lang['Aide'] = 'Help';
+$lang['Informations / Mentions légales'] = 'Information / Legal notices';
+$lang['Contacts'] = 'Contact';
+
+/* Interface texts */
+$lang['Texte indroduction'] = "<strong>FollowDem</strong> is a cartographic web application to track moving objects equipped with a GPS. This application is used by Ecrins national Park to <a href='http://bouquetins.ecrins-parcnational.fr/'>follow ibex.</a>.<br / ><br / >
+<strong>Follow the movements of this tracked animals.</strong>";
+$lang['Entete liste tracked_objects'] = '<strong>Click the name of a tracked object to see its path</strong> <br /> During the';
+$lang['Slide derniers jours'] = 'last days.';
+$lang['Entete selection periode'] = '<strong>Select the period of travel to display</strong>';
+$lang['Fermer'] = 'Close';
+
+$lang['Nom'] = 'Name';
+$lang['Date'] = 'Date';
+$lang['Altitude'] = 'Altitude (m)';
+$lang['Temperature'] = 'Temperature (°C)';
+
+/*Error messages*/
+$lang['error_10'] = 'A consistency error appeared in the database';
+$lang['Page non trouvee'] = '<center><br /><br /><h1><strong>404 ERROR</strong></h1><br />We regret to inform you that the requested page was not found.</center>';
+$lang['error_ajax'] = 'An error appeared while loading datas.<br />Please try again.<br />
+If the problem persists, thanks to contact us through our website : <a href="http://www.ecrins-parcnational.fr/nous-contacter">www.ecrins-parcnational.fr</a>';
