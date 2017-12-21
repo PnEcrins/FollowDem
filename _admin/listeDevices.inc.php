a01819f0c88153483a3ca8e8de41d5156f40599d
JeanLenormand <jlenormand@gmail.com>
2017-12-21 17:21:20 +0100

    MAJ admin avec pages correspondant à nouvelle BDD

---
+++ _admin/listeDevices.inc.php
@@ -0,0 +1,109 @@
+<?php
+include ("verification.inc.php");
+include ("head.inc.php");
+include ("nav.inc.php");
+
+$db=db::get();
+
+// Suppression
+if(isset($_GET['btSupprimer'])){
+	$reqSupprObj = $db->prepare('DELETE FROM '.config::get('db_prefixe').'devices where id_device = :id');
+	$reqSupprObj->execute(array('id' => $_GET['btSupprimer']));
+	$reqSupprProp = $db->prepare('DELETE FROM '.config::get('db_prefixe').'devices where id_device = :id_device');
+	$reqSupprProp->execute(array('id_device' => $_GET['btSupprimer']));
+}
+
+$cpt = 0;
+if (isset($_GET['btSuivant'])){
+	$nbDepart = 15;
+	$nbDepart = $_GET['btSuivant'];
+	$cpt = $nbDepart + 15;
+	$db=db::get();
+	$reqObj = $db->prepare('SELECT * FROM '.config::get('db_prefixe').'devices LIMIT '.$cpt.',15');
+	$reqObj->execute();
+	$resultObj = $reqObj->fetchAll();
+}
+else{
+	if (isset($_GET['btPrecedent'])){
+	$nbDepart2 = 0;
+	$nbDepart2 = $_GET['btPrecedent'];
+	if ($nbDepart2 <= 0){
+		$cpt = 0;
+		$disable2 = "disabled";
+	}
+	else{
+		$cpt = $nbDepart2 - 15;
+		$disable2 = "";
+	}
+	$db=db::get();
+	$reqObj = $db->prepare('SELECT * FROM '.config::get('db_prefixe').'devices LIMIT '.$cpt.',15');
+	$reqObj->execute();
+	$resultObj = $reqObj->fetchAll();
+	}
+	else{
+		$db=db::get();
+		$reqObj = $db->prepare('SELECT * FROM '.config::get('db_prefixe').'devices LIMIT 0,15');
+		$reqObj->execute();
+		$resultObj = $reqObj->fetchAll();
+	}
+}
+?>
+<div id="decale">
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-4">
+			<h3>Contenu de la table 'devices'</h3>
+		</div>
+		<div class="col-md-1"></div>
+		<div class="col-md-4">
+			<form class="navbar-form" role="rechercher">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Rechercher" name="rechercher" id="idRechercher">
+					<div class="input-group-btn">
+						<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-1"></div>	
+	</div>	
+	<div class="col-md-1"></div>
+	<div class="col-md-10">	
+	<table class="table table-striped table-bordered table-hover table-condensed">
+		<tr><th></th><th></th><th></th><th>Id Device</th><th>Ref Device</th><th>Device Type</th><th>Device Info</th></tr>
+		<?php foreach($resultObj as $row){ ?>
+			<tr>
+				<td>
+					<form name="formSuppr" action="listeDevices.inc.php" method="GET"><button type="submit" class="btn btn-default" name="btSupprimer" value="<?php echo $row['id']; ?>"><span class="glyphicon glyphicon-trash"></span></button></form>
+				</td>
+				<td>
+					<form name="formModif" action="saisieObj.php" method="GET"><button type="submit" class="btn btn-default" name="btModifier" value="<?php echo $row['id']; ?>"><span class="glyphicon glyphicon-pencil"></span></button></form>			
+				</td>
+				<td>	
+					<form name="formDetail" action="detailObj.php" method="GET"><button type="submit" class="btn btn-default" name="btDetails" value="<?php echo $row['id']; ?>"><span class="glyphicon glyphicon-info-sign"></span></button></form>
+				</td>
+				<td><?php echo $row['id_device']; ?></td>
+				<td><?php echo $row['ref_device']; ?></td>
+                <td><?php echo $row['id_device_type']; ?></td>
+                <td><?php echo $row['ref_device']; ?></td>
+			</tr>
+		<?php } ?>
+	</table>
+	</div>
+	<div class="col-md-1"></div>
+		<form action="" method="GET" class="form-horizontal" name="monForm">
+		<div class="form-group">
+			<div class="col-md-2"></div>
+			<div class="col-md-offset-2 col-md-2">
+				<button type="submit" name="btPrecedent" value="<?php echo $cpt; ?>" id="btPrecedent" class="btn btn-primary btn-lg btn-block <?php echo $disable2; ?>">Précédent</button>
+			</div>
+			<div class="col-md-2">
+				<button type="submit" name="btSuivant" value="<?php if(count($resultLog) < 15){ echo($nbDepart); $disable="disabled"; }else{ echo $cpt; $disable="";} ?>" id="btSuivant" class="btn btn-primary btn-lg btn-block <?php echo $disable; ?>">Suivant</button>
+			</div>
+			<div class="col-md-3"></div>
+		</div>
+	</form>
+</div>
+<?php
+include ("bottom.inc.php");
+?>
--- _admin/nav.inc.php
+++ _admin/nav.inc.php
@@ -9,12 +9,12 @@
 	</div>
 	<div>
 		<ul class="nav navbar-nav">
-			<li><a href="./listeObj.inc.php?btPrecedent=0"><i class="couleurBlanche glyphicon glyphicon-align-left"></i>&nbsp;&nbsp; Objets</a></li>
+			<li><a href="./listeAnimals.inc.php?btPrecedent=0"><i class="couleurBlanche glyphicon glyphicon-align-left"></i>&nbsp;&nbsp; Animals</a></li>
 			<!-- <li><a href="./listeProp.inc.php"><i class="couleurBlanche glyphicon glyphicon-align-center"></i>&nbsp;&nbsp; Propriétés</a></li>
 			<li><a href="./listeData.inc.php"><i class="couleurBlanche glyphicon glyphicon-align-right"></i>&nbsp;&nbsp; Donnees</a></li> -->
-			<li><a href="./listeLogs.inc.php?btPrecedent=0"><i class="couleurBlanche glyphicon glyphicon-eye-open"></i>&nbsp;&nbsp; Logs</a></li>
-			<li><a href="./saisieObj.php"><i class="couleurBlanche glyphicon glyphicon-plus"></i>&nbsp;&nbsp; Nouvel objet</a></li>
-			<li><a href="./aide.php"><i class="couleurBlanche glyphicon glyphicon-question-sign"></i>&nbsp;&nbsp; Aide</a></li>
+			<li><a href="./listeDevices.inc.php?btPrecedent=0"><i class="couleurBlanche glyphicon glyphicon-eye-open"></i>&nbsp;&nbsp; Devices</a></li>
+			<li><a href="./saisieObj.php"><i class="couleurBlanche glyphicon glyphicon-plus"></i>&nbsp;&nbsp; New animal</a></li>
+			<li><a href="./aide.php"><i class="couleurBlanche glyphicon glyphicon-question-sign"></i>&nbsp;&nbsp; Help</a></li>
 		</ul>
 	</div>
 </nav>
