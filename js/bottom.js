db7b280fbe8af497aaf73f2a0f57d48ca2d5e14f
cdcvidal <celine_vidal@natural-solutions.eu>
2020-03-05 14:34:53 +0100

    check if marker id exists

--- js/bottom.js
+++ js/bottom.js
@@ -16,7 +16,7 @@
 			//$('#sidebar').toggleClass('col-sm-4 col-lg-4 col-sm-12 col-lg-12');
 		};
 	});
-	
+
 /*
 	$(window).bind( 'orientationchange', function(e){
 		if ($.event.special.orientationchange.orientation() == "portrait") {
@@ -25,8 +25,8 @@
 			alert('landscape');
 		}
 	});*/
-	
-	
+
+
 	$(document).ajaxSend(function(event, request, settings) {
 		$('#loading').show();
 	});
@@ -34,11 +34,11 @@
 	$(document).ajaxComplete(function(event, request, settings) {
 		$('#loading').hide();
 	});
-	
-	
+
+
 	map.whenReady(function() {$('#loading').hide();});
-	
-	
+
+
 	$(window).resize(function() {
 		$('.tt-dropdown-menu').css('max-height', $('#container').height()-$('.navbar').height()-20);
 	});
@@ -46,23 +46,23 @@
 	$('a.toggle').click(function() {
 		oc_sidebar();
 	});
-	
+
 	/* Open/Close sidebar */
 	function oc_sidebar()
 	{
 		$('a.toggle i').toggleClass('icon-chevron-left icon-chevron-right');
-		$('#map').toggleClass('col-xs-12 col-sm-12 col-md-7 col-lg-8 col-xs-12 col-sm-12 col-md-12 col-lg-12'); 
+		$('#map').toggleClass('col-xs-12 col-sm-12 col-md-7 col-lg-8 col-xs-12 col-sm-12 col-md-12 col-lg-12');
 		//col-sd-12 col-md-7 col-sm-8 col-lg-8 col-sd-12 col-md-12 col-sm-12 col-lg-12');
 		if (document.body.clientWidth < 992) {
 			//$('#sidebar').toggleClass('col-sm-4 col-lg-4 col-sm-12 col-lg-12');
-			$('#map').toggle();	
+			$('#map').toggle();
 		}
 		else
 		{
 			//on affiche la carte si l'écran est redim entre temps
 			if($('#map').css('display') == 'none')
 			{
-				$('#map').show();	
+				$('#map').show();
 				$('#sidebar').toggleClass('col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xs-12 col-sm-12 col-md-7 col-lg-8');
 			}
 		}
@@ -82,23 +82,23 @@
 */
 	/*identifiant actuel de l'objet*/
 	var id = 0;
-	
+
 	/*Periode actuelle d'analyse*/
 	var periode = 0;
-	
+
 	/*Clique sur la liste des objets... Affiche la popup */
 	$('.tracked_objects').click(function(){
 		id = $(this).attr('id');
 		active_tracked_objects(id);
 		active_parcours();
 	});
-	
+
 	/*Active le parcours selon l'ID de l'objet et la période */
 	function active_parcours(closeSide)
 	{
 		//closeSide = typeof closeSide !== 'undefined' ? closeSide : true;
 		//map.setView(latlng,12);
-	
+
 		$('#loading').show();
 		/*On ferme la sidebar si sur mobile et on affiche la carte*/
 		if (document.body.clientWidth <= 992) {
@@ -111,33 +111,34 @@
 			$('a.toggle i').toggleClass('icon-chevron-left icon-chevron-right');
 			map.invalidateSize();
 		};*/
-		
+
 		periode = $("#periode").val();
-		markers[id].openPopup();
-		
-		var latlonmarker = markers[id].getLatLng();
-		
-		
+
+		if(markers[id]) {
+			markers[id].openPopup();
+			var latlonmarker = markers[id].getLatLng();
+		}
+
 		/*On récupère les tracés et les points lié à la période*/
 		$.ajaxSetup({async: false});
 		get_geoJson(id,'Line');
 		get_geoJson(id,'Point');
 		$.ajaxSetup({async: true});
-		
+
 		//map.fitBounds(geojsonLayerLine.getBounds());
 		map.setView(latlonmarker,14);
-		
-		//var latlon = markers[id].getLatLng(); 
+
+		//var latlon = markers[id].getLatLng();
 		//map.panTo(latlon);
 		//map.invalidateSize();
-		
-		
+
+
 		$('#voirparcours'+id).hide();
 
 		return false;
 	}
 
-	
+
 	/* Active un objet dans la liste */
 	function active_tracked_objects(id_tracked_objects)
 	{
@@ -145,22 +146,22 @@
 		 $('.tracked_objects').removeClass('select_tracked_objects');
 		 $('#'+id).addClass('select_tracked_objects');
 	}
-	
+
 	/*Gestion modal initialisation contenu*/
-	
+
 	$('#modalepage').on('hidden.bs.modal', function (e) {
 		$('#modalepage').removeData('bs.modal');
 		$(e.target).html('');
 		//$(e.target).removeData('bs.modal');
 	});
-	
+
 	$('#modalepage').on('shown.bs.modal', function (e) {
 		$('#modalepage').animate({ scrollTop: 0 }, 'slow');
 		$(e.target).scrollTop(0);
 		$('#modalepage').scrollTop(0);
 		//$(document).scrollTop(0);
 	});
-	
+
 	$("#periode").change(function () {
 		active_parcours();
 		/*
@@ -178,7 +179,7 @@
 *
 *
 */
-	
+
 /* id : id de l'objet - type : type de données(Line, Point) - periode : période au format PjourD*/
 function get_geoJson(id,type){
 	  var jsonGet = $.getJSON('/controler/get_parcours_geojson/id_tracked_objects-' + id + '-type-' + type + '-periode-' + periode, function(data) {
@@ -187,7 +188,7 @@ function get_geoJson(id,type){
 			markers[id].bringToFront();
 		}) .done(function() {
 				$('#loading').hide();
-				
+
 			})
 			.fail(function() {
 				$('#loading').hide();
@@ -196,7 +197,7 @@ function get_geoJson(id,type){
 			.always(function() {
 				/*$('#loading').hide();*/
 			});
-			
+
 		jsonGet.complete(function() {
 			$('#loading').hide();
 			markers[id].bringToFront();
@@ -206,13 +207,13 @@ function get_geoJson(id,type){
 			//map.setView(latlonmarker,14)
 			//map.invalidateSize();
 			//map.panInsideBounds(eval('geojsonLayer'+type).getBounds());
-			
+
 			//map.panTo(latlon);
 			//map.invalidateSize();
 		});
 	}
 /** Fin Chargement de donnée GeoJson en Ajax */
-	
+
 
 /**
 * Leafleat paramètres complémentaires
@@ -220,9 +221,9 @@ function get_geoJson(id,type){
 *
 */
 
-	/* 
+	/*
 	Exemple pour charger du MBTiles sur la carte avec leaflet
-	Tile - Contours du PNE 
+	Tile - Contours du PNE
 	var boundaries = null;
 	boundaries = boundaries || L.tileLayer('images/mbtiles/limitespne/{z}/{x}/{y}.png')
 	if (!map.hasLayer(boundaries)) {
